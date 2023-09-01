d51c9adf6d3a471013bb20e2fa10e4fd40cb6986
frank leite lemos costa <frankleite090@gmail.com>
2023-09-01 14:59:57 -0300

    Adding the dataset and the read file in this project

--- src/main.cs
+++ src/main.cs
@@ -1,5 +1,6 @@
 //this is a library
 using System;
+using System.IO;
 
 //main function
 
@@ -9,8 +10,35 @@ namespace Interpreter
   {
     static void Main()
     {
-        Lexema lex = new Lexema;
-        lex.token = "<" ;               
+        Lexema lex = new Lexema();
+        string Walkingfile = "./dataset/exemplo.txt";
+
+        try
+        {
+            // Check if file existe
+            if (File.Exists(Walkingfile))
+            {
+                // Open the file using StreamReader
+                using (StreamReader sr = new StreamReader(Walkingfile))
+                {
+                    int caractereLido;
+                    while ((caractereLido = sr.Read()) != -1)
+                    {
+                        char caractere = (char)caractereLido;
+                        // Faça algo com o caractere, por exemplo, exibi-lo na tela
+                        Console.Write(caractere);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("O arquivo não existe.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ocorreu um erro: {ex.Message}");
+        }
     }
   }
 }
