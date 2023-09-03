b96cb08245955b239651641ee8860f52bf5b2e82
frank leite lemos costa <frankleite090@gmail.com>
2023-09-03 17:43:24 -0300

    change the logic of construction the tokens

--- src/main.cs
+++ src/main.cs
@@ -25,48 +25,57 @@ namespace Interpreter
                     bool sensor = false;
                     int counter = 0;
                     int aux_caracter;
-
+                    int state = 1;
 
                     //in here before the read a file i add the check of the a struct have a ;, if yes construction the token and close this function
 
-                    //in here.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
+                    //in here
 
                     while ((caractereLido = sr.Read()) != -1)
                     {
-                        
-                        //if the readed caractere is 32 so the read file is in a space or 10 if the value is \n end 59 if the value is ;
-                        if(caractereLido == 59)
+                        switch(state)
                         {
-                          aux_caracter = caractereLido;
-                          //in here i call the method that construct the token
-                          //--
-                          //in now i call the method and construct a token for ;
-                        }
-                        //if is a letter or undescore
-                        if((caractereLido >= 65 && caractereLido <= 90) || (caractereLido >= 97 && caractereLido <= 122) || caractereLido == 95)
-                        {
-                          lex.token += (char)caractereLido;
-                          
-                          if(47 < (int)lex.token[0] && (int)lex.token[0] < 57 )
-                          {
-                            Console.WriteLine("a err of write, begin with a number and after write a letter.");
-                            return;
-                          }
-                        }
-                        //if is a number:
-                        else if(caractereLido >= 47 && caractereLido <= 57)
-                        {
-                          if((int)lex.token[0] >= 47 && (int)lex.token)
-                          {
-                          
-                          }
+                          case 1:
+                            //if the readed caractere is 59 if the value is ;
+                            if(caractereLido == 10)
+                            { 
+                              aux_caracter = caractereLido;
+                              state = 1;
+                            }else if(caracterLido == 32 || caractereLido == 9 || caracterLido == 13)
+                            {
+                              state = 1;
+                            }else if(caractereLido == 35)//if is a # →  is a comentary
+                            {
+                              state = 2;
+                            }else if(caractereLido == 61 || caractereLido == 62 || caractereLido == 60)//if is a = or > or <.
+                            {
+                              lex.token += (char)caractereLido;
+                              state = 3;
+                            }else if(caractereLido == 33)//if is a !.
+                            {
+                              lex.token += (char)caractereLido;
+                              state = 4;
+                            }else if(caractereLido == 59 || caractereLido == 43 || caractereLido == 45 || caractereLido == 42 || caractereLido == 47 || caractereLido == 37 )
+                            {
+                              lex.token += (char)caractereLido;
+                              state = 7;
+                            }else if((caractereLido >= 65 && caractereLido <= 90) || (caractereLido >= 97 && caractereLido <= 122) || caractereLido == 95)//check if the caracter is a letter or undescore.
+                            {
+                              lex.token += (char)caractereLido;
+                              state = 5;
+                            }else if(caractereLido >= 47 && caractereLido <= 57)
+                            {
+                              lex.token += (char)caractereLido;
+                              state = 6
+                            }
 
-                          lex.token += (char)caractereLido; 
-                          
-                        }
+                            break;
+                        }//end of switch case
 
-                    }
+                  }
+
                 }
+           
             }
             else
             {
